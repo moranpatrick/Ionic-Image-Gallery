8b275c7d28a3ab20de67b7694c83e7b663dd3e29
moranpatrick <patrickmoran121@gmail.com>
2018-01-20 17:10:51 +0000

    Initial Commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,6 +6,7 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  images = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg'];
 
   constructor(public navCtrl: NavController) {
 
